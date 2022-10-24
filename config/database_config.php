26e9bcd063f67c889541e5c0871c521fcea38d4d
Dan25lt <pirf860312@gmail.com>
2022-10-23 18:09:00 -0600

    phps

--- config/database_config.php
+++ config/database_config.php
@@ -2,7 +2,7 @@
 // Se declaran las constantes
 const DB_HOST = "localhost";
 const DB_USERNAME = "root";
-const DB_PASSWORD = "FundableD0ubles";
+const DB_PASSWORD = "1357loCK";
 const DB_DATABASE_NAME = "escuela";
 
 $con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
